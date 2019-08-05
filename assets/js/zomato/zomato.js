08640f312bcc371859e7e1e0634e29253f86d67b
michaelmccarty <mccartyatl@gmail.com>
2019-08-05 12:59:58 -0400

    finished Zomato plugin API

--- assets/js/zomato/zomato.js
+++ assets/js/zomato/zomato.js
@@ -20,22 +20,57 @@
           console.log (response.nearby_restaurants);
 
           for (i=0;i<response.nearby_restaurants.length;i++){
+            let image="https://dummyimage.com/200x200/6b6b6b/00fff7&text=+Restaurant";
+            if (!(response.nearby_restaurants[i].restaurant.thumb==""))
+              image=response.nearby_restaurants[i].restaurant.thumb;
           $("#content").append(`
-          <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="${response.nearby_restaurants[i].restaurant.thumb}" alt="${response.nearby_restaurants[i].restaurant.name}">
+          <div class="card" style="width: 18rem; float: left">
+            <img class="card-img-top" src="${image}" alt="${response.nearby_restaurants[i].restaurant.name}">
             <div class="card-body">
               <h5 class="card-title">${response.nearby_restaurants[i].restaurant.name}</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <p class="card-text">${response.nearby_restaurants[i].restaurant.cuisines}</p>
               <a href="${response.nearby_restaurants[i].restaurant.url}" class="btn btn-primary">Visit</a>
             </div>
           </div>
           `);
+          if (i>=4)break;
           }
           $(".card").attr("align","center");
         }
       });
      };
 
-     showCurrentLocation();
+     function showTargetLocation(lon,lat) {
+
+      $.ajax({
+       dataType: 'json',
+       url: `https://cors-anywhere.herokuapp.com/https://developers.zomato.com/api/v2.1/geocode?lat=${lat}&lon=${lon}`,
+       headers: {
+         'user-key': '805d1e0d6b6671767949881ca3264f44'
+       }
+       ,success: function(response) {
+
+         console.log (response.nearby_restaurants);
+
+         for (i=0;i<response.nearby_restaurants.length;i++){
+           let image="https://dummyimage.com/200x200/6b6b6b/00fff7&text=+Restaurant";
+           if (!(response.nearby_restaurants[i].restaurant.thumb==""))
+             image=response.nearby_restaurants[i].restaurant.thumb;
+         $("#content").append(`
+         <div class="card" style="width: 18rem; float: left">
+           <img class="card-img-top" src="${image}" alt="${response.nearby_restaurants[i].restaurant.name}">
+           <div class="card-body">
+             <h5 class="card-title">${response.nearby_restaurants[i].restaurant.name}</h5>
+             <p class="card-text">${response.nearby_restaurants[i].restaurant.cuisines}</p>
+             <a href="${response.nearby_restaurants[i].restaurant.url}" class="btn btn-primary">Visit</a>
+           </div>
+         </div>
+         `);
+         if (i>=4)break;
+         }
+         $(".card").attr("align","center");
+       }
+     });
+    };
 
 
